683456744b8117d4780eccda62efd69216323078
Sants16 <jvsantosrocha980@gmail.com>
2023-04-04 17:08:20 -0300

    first commit

---
+++ tailwind.config.js
@@ -0,0 +1,15 @@
+/** @type {import('tailwindcss').Config} */
+module.exports = {
+  content: ["./src/**/*.{html, js}"],
+  theme: {
+    fontSize: {
+      xs: "0.5rem",
+      sm: "0.6rem",
+      base: "1rem",
+      lg: "1.125rem",
+      xl: "1.875rem",
+    },
+    extend: {},
+  },
+  plugins: [],
+};
