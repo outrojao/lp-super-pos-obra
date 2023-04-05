21a3b5ad61279bd56de2c6675e4a8595b5ea9835
Sants16 <jvsantosrocha980@gmail.com>
2023-04-05 19:15:07 -0300

    feat: o-que-e and beneficios section

--- tailwind.config.js
+++ tailwind.config.js
@@ -5,8 +5,8 @@ module.exports = {
     fontSize: {
       xs: "0.5rem",
       sm: "0.6rem",
-      base: "1rem",
-      lg: "1.125rem",
+      base: "0.9rem",
+      lg: "1.2rem",
       xl: "1.875rem",
     },
     extend: {},
